4dc5ffe794a79257ea33a5f9a928adef5541e69a
Amit Klein <a3klein@ucsd.edu>
2025-05-08 16:06:44 -0700

    defining a main function

---
+++ src/SegQC/sqc.py
@@ -0,0 +1,14 @@
+from SpaDa import SpatialData
+import fire 
+
+
+def ingest_experiment(exp_name, general_filepaths, segmentation_filepath): 
+    """
+    exp
+
+    """
+    return 
+
+
+def main(): 
+    fire.Fire("ingest_experiment":ingest_experiment)
